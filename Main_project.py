0c6bcdbfd92ab21d29f48ef769a8359c11b70f24
koosha.mehdipoor <koosha.mehdipoor@gmail.com>
2024-09-28 13:04:21 +0200

    first stage of defining menu

--- Main_project.py
+++ Main_project.py
@@ -1,1 +1,29 @@
-print("test again")
+text_menu = ("For adding a book to the list enter a:\n"
+        "For reading info about a boook enter r:\n"
+        "For deleting a book from the list enter d:\n")
+
+
+def menu():
+    user_input = input(text_menu)
+    if user_input == "a":
+        add_book()
+    elif user_input == "r":
+        read_book()
+    elif user_input == "d":
+        delete_book()
+    else:
+        print("The command is wrong try another time.")
+
+
+    
+def add_book():
+    print("we entered to the add book function")
+
+def read_book():
+    print("we entered to the read book function")
+
+def delete_book():
+    print("we entered to the delete funtion")
+
+
+menu()
